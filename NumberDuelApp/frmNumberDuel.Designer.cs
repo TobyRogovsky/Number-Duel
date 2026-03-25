4af115134ff68746642711c093c7b81138964eac
tobyrogovsky <tobyrogovsky@gmail.com>
2026-03-24 20:06:31 -0400

    #3

--- NumberDuelApp/frmNumberDuel.Designer.cs
+++ NumberDuelApp/frmNumberDuel.Designer.cs
@@ -29,29 +29,30 @@
         private void InitializeComponent()
         {
             tblMain = new TableLayoutPanel();
+            lblRule = new Label();
+            lblNumPicked = new Label();
             btnStart = new Button();
             pnlPlayerOpt = new Panel();
-            rbn2Player = new RadioButton();
             rbnPlayComp = new RadioButton();
+            rbn2Player = new RadioButton();
             lblGameStat = new Label();
-            lblNumPicked = new Label();
             pnlButtons = new Panel();
-            btn1 = new Button();
-            btn2 = new Button();
-            btn3 = new Button();
-            btn4 = new Button();
+            btn16 = new Button();
+            btn15 = new Button();
+            btn14 = new Button();
+            btn13 = new Button();
+            btn12 = new Button();
+            btn11 = new Button();
+            btn10 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn6 = new Button();
             btn5 = new Button();
-            bnt6 = new Button();
-            bnt7 = new Button();
-            bnt8 = new Button();
-            bnt9 = new Button();
-            bny10 = new Button();
-            bnt11 = new Button();
-            bnt12 = new Button();
-            bnt13 = new Button();
-            bnt14 = new Button();
-            bnt15 = new Button();
-            bnt16 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
             tblMain.SuspendLayout();
             pnlPlayerOpt.SuspendLayout();
             pnlButtons.SuspendLayout();
@@ -59,9 +60,11 @@
             // 
             // tblMain
             // 
-            tblMain.ColumnCount = 2;
-            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblMain.ColumnCount = 3;
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tblMain.Controls.Add(lblRule, 2, 1);
             tblMain.Controls.Add(lblNumPicked, 1, 1);
             tblMain.Controls.Add(btnStart, 0, 0);
             tblMain.Controls.Add(pnlPlayerOpt, 1, 0);
@@ -74,10 +77,30 @@
             tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
             tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
             tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 60F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tblMain.Size = new Size(800, 450);
             tblMain.TabIndex = 0;
+            // 
+            // lblRule
+            // 
+            lblRule.Dock = DockStyle.Fill;
+            lblRule.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRule.Location = new Point(535, 67);
+            lblRule.Name = "lblRule";
+            lblRule.Size = new Size(262, 112);
+            lblRule.TabIndex = 5;
+            lblRule.Text = "All numbers picked must be either a multiple or a factor of the number picked previously";
+            lblRule.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblNumPicked
+            // 
+            lblNumPicked.Anchor = AnchorStyles.None;
+            lblNumPicked.BorderStyle = BorderStyle.FixedSingle;
+            lblNumPicked.Font = new Font("Bauhaus 93", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNumPicked.Location = new Point(277, 82);
+            lblNumPicked.Name = "lblNumPicked";
+            lblNumPicked.Size = new Size(243, 82);
+            lblNumPicked.TabIndex = 3;
+            lblNumPicked.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // btnStart
             // 
@@ -94,24 +117,14 @@
             // 
             // pnlPlayerOpt
             // 
+            tblMain.SetColumnSpan(pnlPlayerOpt, 2);
             pnlPlayerOpt.Controls.Add(rbnPlayComp);
             pnlPlayerOpt.Controls.Add(rbn2Player);
             pnlPlayerOpt.Dock = DockStyle.Fill;
-            pnlPlayerOpt.Location = new Point(403, 3);
+            pnlPlayerOpt.Location = new Point(269, 3);
             pnlPlayerOpt.Name = "pnlPlayerOpt";
-            pnlPlayerOpt.Size = new Size(394, 61);
+            pnlPlayerOpt.Size = new Size(528, 61);
             pnlPlayerOpt.TabIndex = 1;
-            // 
-            // rbn2Player
-            // 
-            rbn2Player.AutoSize = true;
-            rbn2Player.Location = new Point(55, 16);
-            rbn2Player.Name = "rbn2Player";
-            rbn2Player.Size = new Size(66, 19);
-            rbn2Player.TabIndex = 0;
-            rbn2Player.TabStop = true;
-            rbn2Player.Text = "2 Player";
-            rbn2Player.UseVisualStyleBackColor = true;
             // 
             // rbnPlayComp
             // 
@@ -120,44 +133,46 @@
             rbnPlayComp.Name = "rbnPlayComp";
             rbnPlayComp.Size = new Size(145, 19);
             rbnPlayComp.TabIndex = 1;
-            rbnPlayComp.TabStop = true;
             rbnPlayComp.Text = "Play against Computer";
             rbnPlayComp.UseVisualStyleBackColor = true;
+            // 
+            // rbn2Player
+            // 
+            rbn2Player.AutoSize = true;
+            rbn2Player.Checked = true;
+            rbn2Player.Location = new Point(55, 16);
+            rbn2Player.Name = "rbn2Player";
+            rbn2Player.Size = new Size(66, 19);
+            rbn2Player.TabIndex = 0;
+            rbn2Player.TabStop = true;
+            rbn2Player.Text = "2 Player";
+            rbn2Player.UseVisualStyleBackColor = true;
             // 
             // lblGameStat
             // 
             lblGameStat.Anchor = AnchorStyles.None;
             lblGameStat.BorderStyle = BorderStyle.FixedSingle;
-            lblGameStat.Location = new Point(78, 82);
+            lblGameStat.Font = new Font("Bauhaus 93", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblGameStat.Location = new Point(11, 82);
             lblGameStat.Name = "lblGameStat";
             lblGameStat.Size = new Size(243, 82);
             lblGameStat.TabIndex = 2;
             lblGameStat.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // lblNumPicked
-            // 
-            lblNumPicked.Anchor = AnchorStyles.None;
-            lblNumPicked.BorderStyle = BorderStyle.FixedSingle;
-            lblNumPicked.Location = new Point(478, 82);
-            lblNumPicked.Name = "lblNumPicked";
-            lblNumPicked.Size = new Size(243, 82);
-            lblNumPicked.TabIndex = 3;
-            lblNumPicked.TextAlign = ContentAlignment.MiddleCenter;
-            // 
             // pnlButtons
             // 
-            tblMain.SetColumnSpan(pnlButtons, 2);
-            pnlButtons.Controls.Add(bnt16);
-            pnlButtons.Controls.Add(bnt15);
-            pnlButtons.Controls.Add(bnt14);
-            pnlButtons.Controls.Add(bnt13);
-            pnlButtons.Controls.Add(bnt12);
-            pnlButtons.Controls.Add(bnt11);
-            pnlButtons.Controls.Add(bny10);
-            pnlButtons.Controls.Add(bnt9);
-            pnlButtons.Controls.Add(bnt8);
-            pnlButtons.Controls.Add(bnt7);
-            pnlButtons.Controls.Add(bnt6);
+            tblMain.SetColumnSpan(pnlButtons, 3);
+            pnlButtons.Controls.Add(btn16);
+            pnlButtons.Controls.Add(btn15);
+            pnlButtons.Controls.Add(btn14);
+            pnlButtons.Controls.Add(btn13);
+            pnlButtons.Controls.Add(btn12);
+            pnlButtons.Controls.Add(btn11);
+            pnlButtons.Controls.Add(btn10);
+            pnlButtons.Controls.Add(btn9);
+            pnlButtons.Controls.Add(btn8);
+            pnlButtons.Controls.Add(btn7);
+            pnlButtons.Controls.Add(btn6);
             pnlButtons.Controls.Add(btn5);
             pnlButtons.Controls.Add(btn4);
             pnlButtons.Controls.Add(btn3);
@@ -169,149 +184,165 @@
             pnlButtons.Size = new Size(794, 265);
             pnlButtons.TabIndex = 4;
             // 
-            // btn1
+            // btn16
             // 
-            btn1.Location = new Point(36, 10);
-            btn1.Name = "btn1";
-            btn1.Size = new Size(75, 50);
-            btn1.TabIndex = 0;
-            btn1.Text = "1";
-            btn1.UseVisualStyleBackColor = true;
+            btn16.BackColor = Color.MediumSpringGreen;
+            btn16.Location = new Point(694, 188);
+            btn16.Name = "btn16";
+            btn16.Size = new Size(75, 50);
+            btn16.TabIndex = 19;
+            btn16.Text = "16";
+            btn16.UseVisualStyleBackColor = false;
             // 
-            // btn2
+            // btn15
             // 
-            btn2.Location = new Point(124, 66);
-            btn2.Name = "btn2";
-            btn2.Size = new Size(75, 50);
-            btn2.TabIndex = 5;
-            btn2.Text = "2";
-            btn2.UseVisualStyleBackColor = true;
+            btn15.BackColor = Color.MediumSpringGreen;
+            btn15.Location = new Point(683, 66);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(75, 50);
+            btn15.TabIndex = 18;
+            btn15.Text = "15";
+            btn15.UseVisualStyleBackColor = false;
             // 
-            // btn3
+            // btn14
             // 
-            btn3.Location = new Point(9, 116);
-            btn3.Name = "btn3";
-            btn3.Size = new Size(75, 50);
-            btn3.TabIndex = 6;
-            btn3.Text = "3";
-            btn3.UseVisualStyleBackColor = true;
+            btn14.BackColor = Color.MediumSpringGreen;
+            btn14.Location = new Point(585, 152);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(75, 50);
+            btn14.TabIndex = 17;
+            btn14.Text = "14";
+            btn14.UseVisualStyleBackColor = false;
             // 
-            // btn4
+            // btn13
             // 
-            btn4.Location = new Point(89, 188);
-            btn4.Name = "btn4";
-            btn4.Size = new Size(75, 50);
-            btn4.TabIndex = 7;
-            btn4.Text = "4";
-            btn4.UseVisualStyleBackColor = true;
+            btn13.BackColor = Color.MediumSpringGreen;
+            btn13.Location = new Point(574, 37);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(75, 50);
+            btn13.TabIndex = 16;
+            btn13.Text = "13";
+            btn13.UseVisualStyleBackColor = false;
+            // 
+            // btn12
+            // 
+            btn12.BackColor = Color.MediumSpringGreen;
+            btn12.Location = new Point(485, 204);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(75, 50);
+            btn12.TabIndex = 15;
+            btn12.Text = "12";
+            btn12.UseVisualStyleBackColor = false;
+            // 
+            // btn11
+            // 
+            btn11.BackColor = Color.MediumSpringGreen;
+            btn11.Location = new Point(465, 107);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(75, 50);
+            btn11.TabIndex = 14;
+            btn11.Text = "11";
+            btn11.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            btn10.BackColor = Color.MediumSpringGreen;
+            btn10.Location = new Point(422, 10);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(75, 50);
+            btn10.TabIndex = 13;
+            btn10.Text = "10";
+            btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.MediumSpringGreen;
+            btn9.Location = new Point(368, 167);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(75, 50);
+            btn9.TabIndex = 12;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.MediumSpringGreen;
+            btn8.Location = new Point(327, 77);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(75, 50);
+            btn8.TabIndex = 11;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.MediumSpringGreen;
+            btn7.Location = new Point(255, 206);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(75, 50);
+            btn7.TabIndex = 10;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.MediumSpringGreen;
+            btn6.Location = new Point(220, 128);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(75, 50);
+            btn6.TabIndex = 9;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
             // 
             // btn5
             // 
+            btn5.BackColor = Color.MediumSpringGreen;
             btn5.Location = new Point(243, 10);
             btn5.Name = "btn5";
             btn5.Size = new Size(75, 50);
             btn5.TabIndex = 8;
             btn5.Text = "5";
-            btn5.UseVisualStyleBackColor = true;
+            btn5.UseVisualStyleBackColor = false;
             // 
-            // bnt6
+            // btn4
             // 
-            bnt6.Location = new Point(220, 128);
-            bnt6.Name = "bnt6";
-            bnt6.Size = new Size(75, 50);
-            bnt6.TabIndex = 9;
-            bnt6.Text = "6";
-            bnt6.UseVisualStyleBackColor = true;
+            btn4.BackColor = Color.MediumSpringGreen;
+            btn4.Location = new Point(89, 188);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(75, 50);
+            btn4.TabIndex = 7;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
             // 
-            // bnt7
+            // btn3
             // 
-            bnt7.Location = new Point(255, 206);
-            bnt7.Name = "bnt7";
-            bnt7.Size = new Size(75, 50);
-            bnt7.TabIndex = 10;
-            bnt7.Text = "7";
-            bnt7.UseVisualStyleBackColor = true;
+            btn3.BackColor = Color.MediumSpringGreen;
+            btn3.Location = new Point(9, 116);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 50);
+            btn3.TabIndex = 6;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
             // 
-            // bnt8
+            // btn2
             // 
-            bnt8.Location = new Point(327, 77);
-            bnt8.Name = "bnt8";
-            bnt8.Size = new Size(75, 50);
-            bnt8.TabIndex = 11;
-            bnt8.Text = "8";
-            bnt8.UseVisualStyleBackColor = true;
+            btn2.BackColor = Color.MediumSpringGreen;
+            btn2.Location = new Point(124, 66);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(75, 50);
+            btn2.TabIndex = 5;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
             // 
-            // bnt9
+            // btn1
             // 
-            bnt9.Location = new Point(368, 167);
-            bnt9.Name = "bnt9";
-            bnt9.Size = new Size(75, 50);
-            bnt9.TabIndex = 12;
-            bnt9.Text = "9";
-            bnt9.UseVisualStyleBackColor = true;
-            // 
-            // bny10
-            // 
-            bny10.Location = new Point(422, 10);
-            bny10.Name = "bny10";
-            bny10.Size = new Size(75, 50);
-            bny10.TabIndex = 13;
-            bny10.Text = "10";
-            bny10.UseVisualStyleBackColor = true;
-            // 
-            // bnt11
-            // 
-            bnt11.Location = new Point(465, 107);
-            bnt11.Name = "bnt11";
-            bnt11.Size = new Size(75, 50);
-            bnt11.TabIndex = 14;
-            bnt11.Text = "11";
-            bnt11.UseVisualStyleBackColor = true;
-            // 
-            // bnt12
-            // 
-            bnt12.Location = new Point(485, 204);
-            bnt12.Name = "bnt12";
-            bnt12.Size = new Size(75, 50);
-            bnt12.TabIndex = 15;
-            bnt12.Text = "12";
-            bnt12.UseVisualStyleBackColor = true;
-            // 
-            // bnt13
-            // 
-            bnt13.Location = new Point(574, 37);
-            bnt13.Name = "bnt13";
-            bnt13.Size = new Size(75, 50);
-            bnt13.TabIndex = 16;
-            bnt13.Text = "13";
-            bnt13.UseVisualStyleBackColor = true;
-            // 
-            // bnt14
-            // 
-            bnt14.Location = new Point(585, 152);
-            bnt14.Name = "bnt14";
-            bnt14.Size = new Size(75, 50);
-            bnt14.TabIndex = 17;
-            bnt14.Text = "14";
-            bnt14.UseVisualStyleBackColor = true;
-            // 
-            // bnt15
-            // 
-            bnt15.Location = new Point(683, 66);
-            bnt15.Name = "bnt15";
-            bnt15.Size = new Size(75, 50);
-            bnt15.TabIndex = 18;
-            bnt15.Text = "15";
-            bnt15.UseVisualStyleBackColor = true;
-            // 
-            // bnt16
-            // 
-            bnt16.Location = new Point(694, 188);
-            bnt16.Name = "bnt16";
-            bnt16.Size = new Size(75, 50);
-            bnt16.TabIndex = 19;
-            bnt16.Text = "16";
-            bnt16.UseVisualStyleBackColor = true;
+            btn1.BackColor = Color.MediumSpringGreen;
+            btn1.Location = new Point(36, 10);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(75, 50);
+            btn1.TabIndex = 0;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
             // 
             // frmNumberDuel
             // 
@@ -338,21 +369,22 @@
         private RadioButton rbn2Player;
         private Label lblGameStat;
         private Panel pnlButtons;
-        private Button bnt16;
-        private Button bnt15;
-        private Button bnt14;
-        private Button bnt13;
-        private Button bnt12;
-        private Button bnt11;
-        private Button bny10;
-        private Button bnt9;
-        private Button bnt8;
-        private Button bnt7;
-        private Button bnt6;
+        private Button btn16;
+        private Button btn15;
+        private Button btn14;
+        private Button btn13;
+        private Button btn12;
+        private Button btn11;
+        private Button btn10;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btn6;
         private Button btn5;
         private Button btn4;
         private Button btn3;
         private Button btn2;
         private Button btn1;
+        private Label lblRule;
     }
 }
--- NumberDuelApp/frmNumberDuel.cs
+++ NumberDuelApp/frmNumberDuel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,262 @@ namespace NumberDuelApp
 {
     public partial class frmNumberDuel : Form
     {
+        enum TurnEnum { player1, player2 };
+        TurnEnum currentturn = TurnEnum.player1;
+
+        TurnEnum winner;
+
+        List<Button> lstbuttons;
+        bool gameactive = false;
+        int previousnum = 0;
+        bool playcomputer = false;
+        Color startcolor = Color.MediumSpringGreen;
+        Color usedcolor = Color.Gray;
+        Color wincolor = Color.Gold;
+
+       
         public frmNumberDuel()
         {
             InitializeComponent();
+            lstbuttons = new() { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8,
+                btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16 };
+
+            lstbuttons.ForEach(b => b.Click += Button_Click);
+            btnStart.Click += BtnStart_Click;
+            rbnPlayComp.CheckedChanged += Mode;
+            rbn2Player.CheckedChanged += Mode;
+            DisplayGameStatus();
+
+        }
+
+        private void StartGame()
+        {
+            gameactive = true;
+            currentturn = TurnEnum.player1;
+            playcomputer = rbnPlayComp.Checked;
+            previousnum = 0;
+            lblNumPicked.Text = "";
+
+            lstbuttons.ForEach(b =>
+            {
+                b.Enabled = true;
+                b.BackColor = startcolor;
+            });
+    
+
+            DisplayGameStatus();
+        }
+
+        private void DoTurn()
+        {
+          if (!gameactive)
+            {
+                return;
+            }
+          if (DetectWinner())
+            {
+                return;
+            }
+          SwitchTurn();
+          DisplayGameStatus();
+
+            if (IsComputerTurn())
+            {
+                DoComputerTurn();
+            }
+            
+        }
+
+        private void DoComputerTurn()
+        {
+          var lst = lstbuttons.Where(b => b.Enabled && ValidMove(int.Parse(b.Text))).ToList();
+
+          if (lst.Count == 0)
+            {
+                return ;
+            }
+            Button bestbutton = lst[0];
+            int bestscore = CountMovesLeft(int.Parse(bestbutton.Text), bestbutton);
+            foreach(Button b in lst)
+            {
+                int num = int.Parse(b.Text);
+                int score = CountMovesLeft(num, b);
+
+                if (score == 0)
+                {
+                    HandleMove(b);
+                    return;
+                }
+                if (score < bestscore) 
+                {
+                    bestscore = score;
+                    bestbutton = b;
+                }
+
+            }
+            HandleMove(bestbutton);
+        }
+
+        private int CountMovesLeft(int testnum, Button testbutton)
+        {
+            int count = 0;
+            int original = previousnum;
+            previousnum = testnum;
+
+            foreach(Button b in lstbuttons)
+            {
+                if (b.Enabled && b != testbutton)
+                {
+                    int num = int.Parse(b.Text);
+
+                    if (ValidMove(num))
+                    {
+                        count++;
+                    }
+                } 
+            }
+            previousnum = original;
+            return count;
+        }
+
+        private bool IsComputerTurn()
+        {
+            return playcomputer && currentturn == TurnEnum.player2;
+        }
+
+        private void SwitchTurn()
+        {
+            currentturn = currentturn == TurnEnum.player1 ? TurnEnum.player2 : TurnEnum.player1;
+        }
+
+      
+        private void Mode (object ?  sender, EventArgs e)
+        {
+            if (gameactive)
+            {
+                return;
+            }
+            playcomputer = rbnPlayComp.Checked;
+            DisplayGameStatus();
         }
        
+        private string GetPlayerName(TurnEnum turn)
+        {
+            if (!playcomputer)
+            {
+                return turn.ToString();
+            }
+            if (turn == TurnEnum.player1)
+            {
+                return "Player";
+            }
+            return "Computer";
+        }
+        
+        private void DisplayGameStatus()
+        {
+            string msg = "Click Start to begin game";
+            if (gameactive)
+            {
+                msg = GetPlayerName(currentturn);
+            }
+            else if (previousnum != 0)
+            {
+                msg = "Winner: " + GetPlayerName(winner) + " !";
+            }
+
+            msg = (playcomputer ? rbnPlayComp.Text : rbn2Player.Text) + Environment.NewLine + msg;
+
+            lblGameStat.Text = msg;
+        }
+
+        private bool ValidMove(int num)
+        {
+            if (previousnum == 0)
+            {
+                return true;
+            }
+            int sum = num;
+            while (sum <= previousnum)
+            {
+                if (sum == previousnum)
+                {
+                    return true;
+                }
+                sum = sum + num;
+            }
+
+            sum = previousnum;
+            while (sum <= num)
+            {
+                if (sum == num)
+                {
+                    return true;
+                }
+                sum = sum + previousnum;
+            }
+            return false;
+        }
+
+        private bool DetectWinner()
+        {
+            foreach(Button b in lstbuttons)
+            {
+                if (b.Enabled)
+                {
+                    int num = int.Parse(b.Text);
+                    if (ValidMove(num))
+                    {
+                        return false;
+                    }
+                }
+            }
+            gameactive = false;
+            winner = currentturn;            
+            lstbuttons.ForEach(b =>
+            {
+                if (!b.Enabled)
+                {
+                    b.BackColor = wincolor;
+                }
+            });
+
+            DisplayGameStatus();
+            return true;           
+            
+        }
+
+        private void HandleMove(Button btn)
+        {
+            int num = int.Parse(btn.Text);
+
+            if (!ValidMove(num))
+            {
+                return;
+            }
+            previousnum = num;
+            lblNumPicked.Text = "Number: " + btn.Text;
+            btn.Enabled = false;
+            btn.BackColor = usedcolor;
+
+            DoTurn();
+
+        }
+       
+        private void Button_Click(object? sender, EventArgs e)
+        {
+          if(!gameactive)
+                return;
+
+          if (sender is Button btn)
+            {
+                HandleMove(btn);
+            }
+        }
+
+        private void BtnStart_Click(object? sender, EventArgs e)
+        {
+            StartGame();
+        }
     }
 }
